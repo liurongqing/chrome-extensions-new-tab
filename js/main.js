50de4b831edf8a2b21c212df248b2c3f8f066969
liurongqing <liurongqing@zhihuiqiche.com>
2023-09-23 11:57:10 +0800

    feat: 快速提交

--- js/main.js
+++ js/main.js
@@ -16,30 +16,71 @@
       {
         name: "filelist",
         tipPosition: "s",
-        tip: "成为赞助者",
-        icon: `<svg t="1695289971561" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="7206" width="128" height="128"><path d="M994 483H208c-16.5 0-30 13.5-30 30s13.5 30 30 30h786c16.5 0 30-13.5 30-30s-13.5-30-30-30z" fill="#141414" p-id="7207"></path><path d="M35 513m-35 0a35 35 0 1 0 70 0 35 35 0 1 0-70 0Z" fill="#141414" p-id="7208"></path><path d="M994 831.875H214.992c-16.5 0-30 13.5-30 30s13.5 30 30 30H994c16.5 0 30-13.5 30-30s-13.5-30-30-30z" fill="#141414" p-id="7209"></path><path d="M35 862m-35 0a35 35 0 1 0 70 0 35 35 0 1 0-70 0Z" fill="#141414" p-id="7210"></path><path d="M214.992 194.125H994c16.5 0 30-13.5 30-30s-13.5-30-30-30H214.992c-16.5 0-30 13.5-30 30s13.5 30 30 30z" fill="#141414" p-id="7211"></path><path d="M35 164m-35 0a35 35 0 1 0 70 0 35 35 0 1 0-70 0Z" fill="#141414" p-id="7212"></path></svg>`,
+        tip: "文档",
+        icon: `<?xml version="1.0" standalone="no"?>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 1024 1024">
+          <path d="M408 442h480c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8H408c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8zm-8 204c0 4.4 3.6 8 8 8h480c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8H408c-4.4 0-8 3.6-8 8v56zm504-486H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 632H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zM115.4 518.9L271.7 642c5.8 4.6 14.4.5 14.4-6.9V388.9c0-7.4-8.5-11.5-14.4-6.9L115.4 505.1a8.74 8.74 0 0 0 0 13.8z"/>
+        </svg>
+        `,
         click() {
-          alert("捐赠地址：https://ld246.com/sponsor");
+          // console.log('设置icon', )
+          // const filelist = window.vditor.vditor.options.toolbar.find(item => item.name === 'filelist')
+          // if(filelist) {
+          //   filelist.icon = 'xxx'
+          //   console.log(window.vditor.vditor.options.toolbar.find(item => item.name === 'filelist'))
+          // }
+          // this.icon = 'xxx'
         },
       },
       {
         name: "outline",
         tip: "大纲",
+        icon: `<?xml version="1.0" standalone="no"?>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 1024 1024">
+          <path d="M264 230h496c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8H264c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8zm496 424c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8H264c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h496zm144 140H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0-424H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8z"/>
+        </svg>
+        `,
         tipPosition: "s",
       },
       {
         name: "preview",
+        icon: `<?xml version="1.0" standalone="no"?>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 1024 1024">
+          <path d="M942.2 486.2C847.4 286.5 704.1 186 512 186c-192.2 0-335.4 100.5-430.2 300.3a60.3 60.3 0 0 0 0 51.5C176.6 737.5 319.9 838 512 838c192.2 0 335.4-100.5 430.2-300.3 7.7-16.2 7.7-35 0-51.5zM512 766c-161.3 0-279.4-81.8-362.7-254C232.6 339.8 350.7 258 512 258c161.3 0 279.4 81.8 362.7 254C791.5 684.2 673.4 766 512 766zm-4-430c-97.2 0-176 78.8-176 176s78.8 176 176 176 176-78.8 176-176-78.8-176-176-176zm0 288c-61.9 0-112-50.1-112-112s50.1-112 112-112 112 50.1 112 112-50.1 112-112 112z"/>
+        </svg>
+        `,
         tip: "预览",
         tipPosition: "s",
         className: "flex-1",
       },
+
       {
         name: "delete",
+        tip: "删除",
+        tipPosition: "w",
+        confirm() {
+          console.log("点击了确定，点击");
+        },
+      },
+      {
+        name: "content-theme",
+
+        icon: `<?xml version="1.0" standalone="no"?>
+      <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 1024 1024">
+        <path d="M870 126H663.8c-17.4 0-32.9 11.9-37 29.3C614.3 208.1 567 246 512 246s-102.3-37.9-114.8-90.7a37.93 37.93 0 0 0-37-29.3H154a44 44 0 0 0-44 44v252a44 44 0 0 0 44 44h75v388a44 44 0 0 0 44 44h478a44 44 0 0 0 44-44V466h75a44 44 0 0 0 44-44V170a44 44 0 0 0-44-44zm-28 268H723v432H301V394H182V198h153.3c28.2 71.2 97.5 120 176.7 120s148.5-48.8 176.7-120H842v196z"/>
+      </svg>
+      `,
+        tip: "主题",
+        tipPosition: "w",
       },
       {
         name: "export",
-        tip: "导出",
-        tipPosition: "s",
+        icon: `<?xml version="1.0" standalone="no"?>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" fill="currentColor" fill-rule="evenodd">
+          <path d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h360c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8H184V184h656v320c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8V144c0-17.7-14.3-32-32-32ZM770.87 824.869l-52.2 52.2c-4.7 4.7-1.9 12.8 4.7 13.6l179.4 21c5.1.6 9.5-3.7 8.9-8.9l-21-179.4c-.8-6.6-8.9-9.4-13.6-4.7l-52.4 52.4-256.2-256.2c-3.1-3.1-8.2-3.1-11.3 0l-42.4 42.4c-3.1 3.1-3.1 8.2 0 11.3l256.1 256.3Z" transform="matrix(1 0 0 -1 0 1024)"/>
+        </svg>
+        `,
+        tipPosition: "w",
       },
     ],
     preview: {
@@ -60,5 +101,5 @@
     height: "100vh",
     // placeholder: "Take me notes in your new tab of browser."
   };
-  new Vditor("vditor", window.options);
+  window.vditor = new Vditor("vditor", window.options);
 })();
