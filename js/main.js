ad512deb4dfcf706106a34239c65b03b2742cc0a
liurongqing <liurongqing@zhihuiqiche.com>
2023-10-11 00:51:28 +0800

    feat: 快速提交

--- js/main.js
+++ js/main.js
@@ -117,7 +117,7 @@ const Storage = {
       {
         name: "preview",
         icon: previewIcon,
-        tip: "进入预览",
+        tip: "预览",
         tipPosition: "s",
         className: "preview-container",
         afterClick() {
@@ -131,20 +131,20 @@ const Storage = {
           }
         },
       },
-      {
-        name: "delete001",
-        tipPosition: "s",
-        icon: `<?xml version="1.0" standalone="no"?>
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 1024 1024">
-          <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"/>
-          <path d="M623.6 316.7C593.6 290.4 554 276 512 276s-81.6 14.5-111.6 40.7C369.2 344 352 380.7 352 420v7.6c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8V420c0-44.1 43.1-80 96-80s96 35.9 96 80c0 31.1-22 59.6-56.1 72.7-21.2 8.1-39.2 22.3-52.1 40.9-13.1 19-19.9 41.8-19.9 64.9V620c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8v-22.7a48.3 48.3 0 0 1 30.9-44.8c59-22.7 97.1-74.7 97.1-132.5.1-39.3-17.1-76-48.3-103.3zM472 732a40 40 0 1 0 80 0 40 40 0 1 0-80 0z"/>
-        </svg>
-        `,
-        click() {
-          console.log("清除成功");
-          Storage.clear();
-        },
-      },
+      // {
+      //   name: "delete001",
+      //   tipPosition: "s",
+      //   icon: `<?xml version="1.0" standalone="no"?>
+      //   <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 1024 1024">
+      //     <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"/>
+      //     <path d="M623.6 316.7C593.6 290.4 554 276 512 276s-81.6 14.5-111.6 40.7C369.2 344 352 380.7 352 420v7.6c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8V420c0-44.1 43.1-80 96-80s96 35.9 96 80c0 31.1-22 59.6-56.1 72.7-21.2 8.1-39.2 22.3-52.1 40.9-13.1 19-19.9 41.8-19.9 64.9V620c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8v-22.7a48.3 48.3 0 0 1 30.9-44.8c59-22.7 97.1-74.7 97.1-132.5.1-39.3-17.1-76-48.3-103.3zM472 732a40 40 0 1 0 80 0 40 40 0 1 0-80 0z"/>
+      //   </svg>
+      //   `,
+      //   click() {
+      //     console.log("清除成功");
+      //     Storage.clear();
+      //   },
+      // },
       {
         name: "help",
         tipPosition: "s",
@@ -155,10 +155,6 @@ const Storage = {
         </svg>
         `,
         className: "flex-1",
-        click() {
-          console.log("清除成功");
-          Storage.clear();
-        },
       },
       {
         name: "addfile",
@@ -376,6 +372,15 @@ const Storage = {
     // 初始化数据，第一次进来没有数据，则默认给个模板
     const { list } = await Storage.get("list");
 
+    // 预览文案设置
+    const { setting_preview } = await Storage.get("setting_preview");
+    const button = document.querySelector(".preview-container button");
+    if (setting_preview) {
+      button.setAttribute("aria-label", "取消预览，进入编辑。");
+    } else {
+      button.setAttribute("aria-label", "进入预览");
+    }
+
     if (!list) {
       add();
       return;
@@ -389,8 +394,6 @@ const Storage = {
     if (!localCurrent) {
       current = list[0].id;
     }
-
-    
   }
 
   // 选中当前内容
@@ -398,7 +401,7 @@ const Storage = {
     if (current) {
       const contentObj = await Storage.get(current);
       const content = Object.values(contentObj);
-      window.vditor.setValue(content[0] ?? '', true);
+      window.vditor.setValue(content[0] ?? "", true);
       window.vditor.focus();
     }
   }
